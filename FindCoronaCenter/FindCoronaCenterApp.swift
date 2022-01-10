2aa1a2af58357d0f63656beeb0854e444a3fd911
soduma <soduma@naver.com>
2022-01-10 14:10:01 +0900

    Initial Commit

---
+++ FindCoronaCenter/FindCoronaCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindCoronaCenterApp.swift
+//  FindCoronaCenter
+//
+//  Created by 장기화 on 2022/01/10.
+//
+
+import SwiftUI
+
+@main
+struct FindCoronaCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
